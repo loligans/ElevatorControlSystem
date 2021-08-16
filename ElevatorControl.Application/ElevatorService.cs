bae9ded3eb1ff535bcffcd08d5c1435a3fbd57c2
Ethan Lindemann <ethan.lindemann@outlook.com>
2021-08-15 20:28:44 -0700

    Added a structured ElevatorControlSystem solution

---
+++ ElevatorControl.Application/ElevatorService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ElevatorControl.Application
+{
+    /// <summary>
+    /// The elevator service contract
+    /// </summary>
+    public interface IElevatorService
+    {
+
+    }
+
+    internal class ElevatorService : IElevatorService
+    {
+    }
+}
---
+++ ElevatorControl.Application/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ElevatorControl.Application.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection serviceCollection)
+        {
+            throw new NotImplementedException();
+            return serviceCollection;
+        }
+    }
+}
